4665ba0355829a64ff30c233e93a1724172f6cad
jibola <pro.ajibolaojo@gmail.com>
2021-11-23 11:53:10 +0100

    shop page started

---
+++ components/ShoppingCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+
+const ShoppingCard = () => {
+  return (
+    <div
+      className="bg-white rounded-xl border mt-9 relative text-center text-sm select-none flex flex-col justify-center"
+      style={{ height: "226px", width: "192px" }}
+    >
+      <span className="block">
+        <Image alt="" src="/images/food.png" height={100} width={100} />
+      </span>
+      <div className="mx-auto" style={{ width: "144px" }}>
+        <div className="font-medium mb-1">Carton of Indomie</div>
+        <div>N3,500</div>
+        <div className="text-xs mb-4"> 20 packs available</div>
+        <button style={{backgroundColor: "#251F2D"}} className="text-white text-sm font-semibold px-3 py-1 rounded"> Add to Cart</button>
+      </div>
+    </div>
+  );
+};
+
+export default ShoppingCard;
--- pages/shop.js
+++ pages/shop.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { useQuery } from "react-query";
 import { getMe } from "../actions/auth";
 import DashboardTemplate from "../components/DashboardTemplate";
+import ShoppingCard from "../components/ShoppingCard";
 import { redirectLoggedOut } from "../utils/utils";
 
 export default function Shop() {
@@ -19,6 +20,23 @@ export default function Shop() {
         loading={getMeQuery.isLoading}
         success={getMeQuery.isSuccess}
       >
+        <>
+          <div className="dashboard-container py-10">
+            <div className="font-semibold text-xl">All Items</div>
+            <div className="mb-4 flex justify-end">
+              <span className="font-semibold cursor-pointer">View All</span>
+            </div>
+            <div className="max-w-full overflow-x-auto">
+              <div className="grid gap-4  grid-flow-col  grid-rows-1 auto-cols-auto mb-9">
+                <ShoppingCard />
+                <ShoppingCard />
+                <ShoppingCard />
+                <ShoppingCard />
+                <ShoppingCard />
+              </div>
+            </div>
+          </div>
+        </>
       </DashboardTemplate>
     </div>
   );
